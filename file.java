78a93b929371f5b59420c598b5cfb6988dad9f93
krs1 <salvare000919@gmail.com>
2015-07-03 16:29:01 +0900

    DOMでXMLを編集する形式に変更 変数を入れる順番が違う場合でも対応できるようにした

--- file.java
+++ file.java
@@ -1,5 +1,25 @@
-import java.io.*;
-import java.util.*;//ArrayListとScaner
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+import javax.naming.directory.Attribute;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Element;
+import org.w3c.dom.Document;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
+
 public class file{
 	public static void main(String args[]){
 		
@@ -22,7 +42,7 @@ public class file{
 		//Fileクラスのインスタンス化
 		File file = new File(file_name);
 		if(file_test(file)){
-			 // file_Edit(file,para_name,para_number);
+			 //file_Edit(file,para_name,para_number);
 			String editText = file_Edit(file,para_name,para_number);
 			file_Write(file, editText);
 		}
@@ -61,40 +81,38 @@ public class file{
 	}
 	private static String file_Edit(File file_name, ArrayList name, ArrayList number){
 		StringBuffer fileRead = new StringBuffer("");
+		StringBuffer xmlRead = new StringBuffer("");
+		File file = new File("hoge.xml");
 		int i=0;
 		try{
+			file.createNewFile();
 			BufferedReader br = new BufferedReader(new FileReader(file_name));
+			BufferedReader brtmp = new BufferedReader(new FileReader(file));
 			String str = null;
-			while((str=br.readLine()) != null){//1行読み込み
-				 if(str.equals("<experiments>")){//<experiments>に当たったら別の処理
-				 	fileRead.append(str+"\r\n");
-				 	System.out.println(str);
-				 	while(!(str=br.readLine()).equals("</experiments>")){//</experiments>までループ
-				 		if(name.size()>i){
-							if(str.equals("    <enumeratedValueSet variable=\""+name.get(i)+"\">")){//name内の文字列比較
-								fileRead.append(str+"\r\n");//上の文字列をそのまま追加
-								System.out.println(str);
-								br.readLine();//読み込み
-								str="      <value value=\""+number.get(i)+"\"/>\r\n";
-								fileRead.append(str);//文字列の書き換え
-								System.out.print(str);
-								i++;//下の変数の更新
-							}else {
-								fileRead.append(str+"\r\n");
-								System.out.println(str);
-								try{
-									Thread.sleep(1000); //3000ミリ秒Sleepする
-								}catch(InterruptedException e){}
-							}
-						}else{
-							fileRead.append(str+"\r\n");
-							System.out.println(str);
-						}
-						
+			String tmp = null;
+			while((tmp=br.readLine()) != null){//1行読み込み
+				if(tmp.equals("<experiments>")){//<experiments>に当たったら別の処理
+				 	xmlRead.append(tmp+"\r\n");//別枠で保存
+				 	while(!(tmp=br.readLine()).equals("</experiments>")){//</experiments>までループ
+				 		xmlRead.append(tmp+"\r\n");//xmlに追加</experiments>まで追加
 				 	}
-				 }
-				fileRead.append(str+"\r\n");
-				System.out.println(str);
+				 	xmlRead.append(tmp+"\r\n");//</experiments>を追加
+				 	file_Write(file,xmlRead.toString());//hogeに書き込み
+				 	xmlEdit(file, name, number);//hogeを編集
+				 	while((str=brtmp.readLine()) != null){
+				 		if(i==1){
+				 			fileRead.append(str+"\r\n");
+				 		}
+				 		else{
+				 			i++;
+				 		}
+				 	}
+
+				}else{
+					str = tmp;
+					fileRead.append(str+"\r\n");
+					//System.out.println(str);
+				}
 			}
 			br.close();
 		}catch (FileNotFoundException ex){
@@ -118,5 +136,48 @@ public class file{
         } catch ( IOException ex ) {
              System.out.println("106:"+ex);
         }
+	}
+	private static void xmlEdit(File file, ArrayList name, ArrayList number){
+		try{
+			Document document= DocumentBuilderFactory
+       		                 .newInstance()
+   	    	                 .newDocumentBuilder()
+       		                 .parse(new File("hoge.xml"));
+       		NodeList nodeList = document.getElementsByTagName("enumeratedValueSet");//全てのenumeratedValueSetを洗い出し
+       		Element element = document.getDocumentElement();
+       		for(int y=0;y<name.size();y++){
+       			for(int x=0;x<name.size();x++){
+       				if((name.get(y).toString()).equals(nodeList.item(x).getAttributes().item(0).getNodeValue())){//density
+       					 element.getElementsByTagName("value").item(x).getAttributes().item(0).setNodeValue(number.get(y).toString());
+       				}
+       			}
+       		}
+       		Transformer transformer = null;
+   		 	try {
+   		     	TransformerFactory factory = TransformerFactory.newInstance();
+   		     	transformer = factory.newTransformer();
+       		} catch (TransformerConfigurationException e) {
+           		// 通常はありえない。
+           		e.printStackTrace();
+       		}
+       		transformer.setOutputProperty("indent",   "yes");
+       		transformer.setOutputProperty("encoding", "UTF-8");
+       		transformer.setOutputProperty("omit-xml-declaration", "no");
+       		try {
+       		    transformer.transform(new DOMSource(document),
+       		                          new StreamResult(new File("hoge.xml")));
+       		} catch (TransformerException e) {
+       		    // 書き出しエラー発生
+       		    e.printStackTrace();
+       		}
+       	}catch(ParserConfigurationException e){
+       			System.out.println(e);
+       	}catch(SAXException e){
+       		System.out.println("not xml");
+     		e.printStackTrace();
+     	}catch(IOException e){
+     		System.out.println(e);
+     	}
+
 	}
 }
